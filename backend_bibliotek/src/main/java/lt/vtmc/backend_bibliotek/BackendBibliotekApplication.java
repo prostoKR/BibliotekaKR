7b6af548a4a3316965732668b163fc0c03970733
KonstantinR <konstantinrub@gmail.com>
2022-06-28 16:19:10 +0300

    veikiantis backas ir frontas tusti

---
+++ backend_bibliotek/src/main/java/lt/vtmc/backend_bibliotek/BackendBibliotekApplication.java
@@ -0,0 +1,13 @@
+package lt.vtmc.backend_bibliotek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBibliotekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBibliotekApplication.class, args);
+	}
+
+}
---
+++ backend_bibliotek/src/test/java/lt/vtmc/backend_bibliotek/BackendBibliotekApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vtmc.backend_bibliotek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBibliotekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
